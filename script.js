25342ce270842fe64be4bb899daa230d42903280
mrhotsauce124 <mrhotsauce124@hotmail.com>
2021-08-31 19:25:11 +0100

    added player objects and basic interaction

--- script.js
+++ script.js
@@ -31,7 +31,13 @@ const gameBoard = (() => {
 function createGameBox() {
     let gameBox = document.createElement("div");
     gameBox.classList.add("game-box");
-    gameBox.innerHTML = X;
+    gameBox.innerHTML = "";
+    gameBox.addEventListener("click", updateTurn);
+    gameBox.addEventListener("click", updateSymbol);
+
+    function updateSymbol() {
+        gameBox.innerHTML = currentTurn? X : O;
+    }
 
     const setState = (symbol) => {
         gameBox.innerHTML = symbol;
@@ -45,4 +51,70 @@ function createGameBox() {
 }
 
 
+// Factory function for a player object
+
+function createPlayer(ID_number, name = "Anonymous Player") {
+    let ID = ID_number;
+    let currentTurn = ID ? false : true;
+
+    // TODO: change this to use more appropriate elements
+
+    let playerBox = document.createElement("div");
+    playerBox.classList.add(ID ? "player2-box" : "player1-box");
+
+      
+    let playerName = document.createElement("h1");
+    let playerTurn = document.createElement("h2");
+    
+
+    playerBox.appendChild(playerName);
+    playerBox.appendChild(playerTurn);
+    playerName.textContent = name;
+    playerTurn.textContent = currentTurn ? `Your turn` : `Their turn`;
+
+
+    // TODO: add score
+    
+
+
+    const updateInfo = () => {
+        playerName.textContent = name;
+        playerTurn.textContent = currentTurn ? `Your turn` : `Their turn`;
+    };  
+
+    const nextTurn = () => {
+        currentTurn = !currentTurn;
+    };
+    
+    const displayInfo = () => {
+        document.getElementById("main-div").appendChild(playerBox);
+    };
+
+
+    return {ID, currentTurn, displayInfo, nextTurn, updateInfo};
+
+}
+
+
+
+var player1 = createPlayer(0, "Player 1");
+
+player1.displayInfo();
+
 gameBoard.display();
+
+var player2 = createPlayer(1, "Player 2");
+
+player2.displayInfo();
+
+var currentTurn = 0;
+
+
+function updateTurn() {
+    player1.nextTurn();
+    player2.nextTurn();
+    player1.updateInfo();
+    player2.updateInfo();
+    currentTurn = !currentTurn;
+}
+
