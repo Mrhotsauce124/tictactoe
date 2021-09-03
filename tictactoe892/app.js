bf6172b208a961fc6889aa22125c9a52163b2a5a
mrhotsauce124 <mrhotsauce124@hotmail.com>
2021-09-03 03:26:11 +0100

    fixed get request handling

--- tictactoe892/app.js
+++ tictactoe892/app.js
@@ -7,10 +7,22 @@ app.use(express.static('tictactoe892'));
 
 const port = 3000;
 
-app.get('/*', (req, res) => {
+app.get('/', (req, res) => {
     res.statusCode = 200;
     res.sendFile('index.html', {root: __dirname});
 });
+
+app.get('/script.js', (req, res) => {
+    res.statusCode = 200;
+    res.sendFile('script.js', {root: __dirname});
+});
+
+
+app.get('/styles.css', (req, res) => {
+    res.statusCode = 200;
+    res.sendFile('styles.css', {root: __dirname});
+});
+
 
 app.listen(port, () => {
     console.log(`Server running at port ${port}`);
